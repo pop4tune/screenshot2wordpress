ec79a68a681292baae735a9e855ab7c163e99461
Alex Postnikov <postnikov@gmail.com>
2015-08-06 15:27:36 +0300

    drawing

--- v2/scripts/fsCaptured.js
+++ v2/scripts/fsCaptured.js
@@ -53,7 +53,12 @@ window.addEventListener('load', function () {
 	}
 	
 	/***************************************************************************************/
-	
+
+	var iZoom = 1;
+	var context;
+	var canvas;
+	var imgObj;
+
 	function initHandlers()
 	{
 		$("#btnPrint").click(function() {
@@ -70,6 +75,50 @@ window.addEventListener('load', function () {
 			iframe.contentWindow.print(); 
 			$("#fsTempElement").remove();
 		});
+
+		$("#zoomIn").click(function() {
+				//var div = document.getElementById("divImgResult");
+				iZoom = iZoom * 1.25;
+				var w = backgroundPage.capResult.width * iZoom;
+				var h = backgroundPage.capResult.height * iZoom;
+	            canvas.attr({width: w, height: h}).css({'background-size': '' +w + 'px ' +	 h + 'px'});
+				//console.log(context,iZoom);
+				//context.scale(iZoom, iZoom);
+				//context.rotate(45);
+				//context.clearRect(0, 0, 100000, 100000);
+				//context.drawImage(backgroundPage.capResult, 0,0, backgroundPage.capResult.width * iZoom, backgroundPage.capResult.height * iZoom)
+		});
+		$("#zoomOut").click(function() {
+				//var div = document.getElementById("divImgResult");
+				iZoom = iZoom * 0.75;
+				var w = backgroundPage.capResult.width * iZoom;
+				var h = backgroundPage.capResult.height * iZoom;
+	            canvas.attr({width: w, height: h}).css({'background-size': '' +w + 'px ' +	 h + 'px'});
+
+				//div.style.zoom = iZoom;
+				//context.scale(iZoom, iZoom);
+				//canvas.width = backgroundPage.capResult.width * iZoom;
+				//canvas.height = backgroundPage.capResult.height * iZoom;
+				//context.clearRect(0, 0, canvas.width, canvas.height );
+				//context.drawImage(backgroundPage.capResult, 0,0, backgroundPage.capResult.width * iZoom, backgroundPage.capResult.height * iZoom)
+		});
+
+		$("#btnPrint").click(function() {
+			var iframe = document.createElement("IFRAME");
+			
+			$(iframe).attr({
+				style: "width:0px;height:0px;",
+				id: "fsTempElement"
+			});
+			
+			document.body.appendChild(iframe);
+			iframe.contentWindow.document.write("<div style='margin:0 auto;text-align:center'><img style='width:100%' src='" + document.getElementById("imgResult").src + "'></div>");
+		
+			iframe.contentWindow.print(); 
+			$("#fsTempElement").remove();
+		});
+
+
 		
 		$("#lnkOptions").click(function() {
 			backgroundPage.openExtensionPreferences();
@@ -93,6 +142,18 @@ window.addEventListener('load', function () {
 
 		$("#sendBtn").click(function() {
 
+						var getMerged = function() {
+			                // Create the merged image
+			                var mergeCanvas = document.createElement('canvas'),
+			                    mergeContext = mergeCanvas.getContext('2d');
+			                    
+			                    mergeCanvas.width = canvas[0].width;
+			                    mergeCanvas.height = canvas[0].height;
+			                    
+			                    mergeContext.drawImage(imgObj, 0, 0, canvas[0].width, canvas[0].height);
+			                    mergeContext.drawImage(canvas[0], 0, 0);
+			                return mergeCanvas.toDataURL("image/png");
+			            }
                         fd = new FormData();
                         localStorage.login =  'postnikov@gmail.com';
                         localStorage.passwd =  '11111111';
@@ -101,7 +162,7 @@ window.addEventListener('load', function () {
                         fd.append('user', localStorage.login );
                         fd.append('userid', localStorage.userid );
                         fd.append('password', localStorage.passwd );
-                        fd.append('img', document.getElementById("imgResult").src);
+                        fd.append('img', getMerged());//document.getElementById("imgResult").src);
                         fd.append('website', $("#website").val());
                         //fd.append('acl', "public-read");
                         //fd.append('content-type', "image/png");
@@ -167,9 +228,30 @@ window.addEventListener('load', function () {
 	{
 		$(".container").show();
 		
-		document.getElementById("imgResult").src = backgroundPage.capResultDataURL;
+		//document.getElementById("imgResult").src = backgroundPage.capResultDataURL;
+        canvas = $('<canvas />').attr('id', 'imgSketch'),
+            context = canvas[0].getContext('2d'),
+            imgObj = new Image();
+
+		//canvas = $("#imgSketch")[0];
+
+        imgObj.onload = function() {
+	        /*canvas.width = imageObj.width;
+	        canvas.height = imageObj.height;
+	        console.log(imageObj.width, imageObj.height);
+	        //context.drawImage(this, 0, 0);
+	        $("#imgSketch").css({"background-url": backgroundPage.capResultDataURL});
+	        //$('#imgSketch').sketch({defaultColor: "#ff0"});*/
+            canvas.attr({width: this.width, height: this.height}).css({backgroundImage: "url(" + backgroundPage.capResultDataURL + ")", backgroundRepeat:'none'});
+	        canvas.appendTo(document.getElementById("divImgResult"));
+	        canvas.sketch({defaultColor: "#ff0000"});
+        };
+
+
+        imgObj.src = backgroundPage.capResultDataURL;
+
 		document.title = backgroundPage.capResultFileNameLite;//backgroundPage.tabTitle + " (" + backgroundPage.tabURL + ")";
-		
+		return;
 		var img = backgroundPage.capResult;
 		var div = document.getElementById("divImgResult");
 		if (img.width < $("#divImgResult").width())
